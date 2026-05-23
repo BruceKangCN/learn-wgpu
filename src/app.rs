2d86949943b80cd6495fc235aba0e187c5c2737e
Bruce Kang <BruceKangCN@gmail.com>
2026-05-23 09:32:44 +0800

    initial commit

---
+++ src/app.rs
@@ -0,0 +1,223 @@
+use std::sync::Arc;
+
+use anyhow::{Context, Result};
+use pollster::FutureExt;
+use tracing::{debug, error};
+use winit::{
+    application::ApplicationHandler,
+    event::{KeyEvent, WindowEvent},
+    event_loop::ActiveEventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+#[derive(Debug)]
+pub struct State {
+    window: Arc<Window>,
+
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+
+    surface_configured: bool,
+}
+
+impl State {
+    async fn new(window: Arc<Window>) -> Result<Self> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..wgpu::InstanceDescriptor::new_without_display_handle()
+        });
+        let surface = instance
+            .create_surface(window.clone())
+            .context("failed to create surface")?;
+
+        let adapter_options = wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance
+            .request_adapter(&adapter_options)
+            .await
+            .context("failed to create adapter")?;
+
+        let device_desc = wgpu::DeviceDescriptor {
+            label: None,
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::defaults(),
+            experimental_features: wgpu::ExperimentalFeatures::disabled(),
+            memory_hints: wgpu::MemoryHints::Performance,
+            trace: wgpu::Trace::Off,
+        };
+        let (device, queue) = adapter
+            .request_device(&device_desc)
+            .await
+            .context("failed to request for a device")?;
+
+        let caps = surface.get_capabilities(&adapter);
+        let surface_format = caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            surface_configured: false,
+        })
+    }
+
+    fn resize(&mut self, width: u32, height: u32) {
+        if width == 0 || height == 0 {
+            return;
+        }
+
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+        self.surface_configured = true;
+    }
+
+    fn render(&mut self) -> Result<()> {
+        self.window.request_redraw();
+
+        if !self.surface_configured {
+            return Ok(());
+        }
+
+        let output = match self.surface.get_current_texture() {
+            wgpu::CurrentSurfaceTexture::Success(surface_texture) => surface_texture,
+            wgpu::CurrentSurfaceTexture::Suboptimal(surface_texture) => {
+                self.surface.configure(&self.device, &self.config);
+                surface_texture
+            }
+            wgpu::CurrentSurfaceTexture::Timeout
+            | wgpu::CurrentSurfaceTexture::Occluded
+            | wgpu::CurrentSurfaceTexture::Validation => {
+                // skip this frame
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Outdated => {
+                self.surface.configure(&self.device, &self.config);
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Lost => {
+                anyhow::bail!("Lost device");
+            }
+        };
+
+        let texture_view_desc = wgpu::TextureViewDescriptor::default();
+        let view = output.texture.create_view(&texture_view_desc);
+
+        let command_encoder_desc = wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        };
+        let mut encoder = self.device.create_command_encoder(&command_encoder_desc);
+
+        let render_pass_desc = wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                depth_slice: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            ..wgpu::RenderPassDescriptor::default()
+        };
+        let _render_pass = encoder.begin_render_pass(&render_pass_desc);
+        drop(_render_pass);
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        match (code, is_pressed) {
+            (KeyCode::Escape, true) => event_loop.exit(),
+            _ => {}
+        }
+    }
+
+    fn update(&mut self) {
+        // todo!()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    state: Option<State>,
+}
+
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let win_attr = Window::default_attributes().with_title("Learn Wgpu");
+        let win = Arc::new(event_loop.create_window(win_attr).unwrap());
+
+        self.state = Some(State::new(win).block_on().unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.update();
+                if let Err(e) = state.render() {
+                    error!("Error during render: {:?}", e);
+                    event_loop.exit();
+                }
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
+            ev => {
+                debug!(?window_id, ?ev, "other event");
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use anyhow::{Context, Result};
+use tracing_subscriber::EnvFilter;
+use winit::event_loop::EventLoop;
+
+use crate::app::App;
+
+mod app;
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .pretty()
+        .init();
+
+    let event_loop = EventLoop::with_user_event()
+        .build()
+        .context("failed to build event loop")?;
+    let mut app = App::default();
+
+    Ok(event_loop.run_app(&mut app)?)
+}
